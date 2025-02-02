e94ad4552891681261a5735c1b17615035ce433f
pocketcorp <pocketcorp@protonmail.com>
2025-02-01 17:08:01 -0800

    super quality section completed

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -4,7 +4,7 @@ import PopularProductsCard from '../components/PopularProductsCard'
 
 const PopularProducts = () => {
   return (
-    <section className="max-container max-sm:mt-12">
+    <section id='products' className="max-container max-sm:mt-12">
       <div className="flex flex-col justify-start gap-5">
         <h2 className="text-4xl font-palanquin font-bold">
           Our 
